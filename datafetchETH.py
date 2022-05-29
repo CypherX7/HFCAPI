db836f60c327d30cce4d51a17be8910a3c73f938
CypherX7 <cypherx702@gmail.com>
2022-05-29 23:38:21 +0530

    final

--- datafetchETH.py
+++ datafetchETH.py
@@ -3,38 +3,41 @@ import json
 import time
 
 while True:
-    time.sleep(1)
-    w3ETH = Web3(Web3.HTTPProvider("https://speedy-nodes-nyc.moralis.io/db66f798f03c28b4ccf9b81c/eth/mainnet")) 
-    with open('./ABI.json') as f:
-        ABI = json.load(f)
-        f.close()
-        HFCETH = w3ETH.eth.contract(address=Web3.toChecksumAddress('0xe1b2ba089ea5ac932dad7d98b897a895d681d3a6'),abi=ABI)
-        while True:
-            time.sleep(2)
-            for i in range(1,50):
-                pricee = (HFCETH.functions.price().call()/(10**18))
-                totalsupply = (HFCETH.functions.totalSupply().call()/(10**18))
-                with open('./DATA.json','r') as k:
-                    data = (json.load(k))
-                    if (list(data["ETH"]["PRICE"].values())[-1]) != pricee:
-                        data["ETH"]["PRICE"][str(int(time.time()))] = pricee
-                        k.close()
-                        with open('./DATA.json','w') as l:
-                            l.write("")
-                            l.write(json.dumps(data))
-                            print(data)
-                            l.close()
-                    else:
-                        k.close()
-                with open('./DATA.json','r') as m:
-                    data = (json.load(m))
-                    if (list(data["ETH"]["TOTALSUPPLY"].values())[-1]) != totalsupply:
-                        data["ETH"]["TOTALSUPPLY"][str(int(time.time()))] = totalsupply
-                        m.close()
-                        with open('./DATA.json','w') as n:
-                            n.write("")
-                            n.write(json.dumps(data))
-                            print(data)
-                            n.close()
-                    else:
-                        m.close()
+    try:
+        time.sleep(1)
+        w3ETH = Web3(Web3.HTTPProvider("https://speedy-nodes-nyc.moralis.io/db66f798f03c28b4ccf9b81c/eth/mainnet")) 
+        with open('./ABI.json') as f:
+            ABI = json.load(f)
+            f.close()
+            HFCETH = w3ETH.eth.contract(address=Web3.toChecksumAddress('0xe1b2ba089ea5ac932dad7d98b897a895d681d3a6'),abi=ABI)
+            while True:
+                time.sleep(2)
+                for i in range(1,50):
+                    pricee = (HFCETH.functions.price().call()/(10**18))
+                    totalsupply = (HFCETH.functions.totalSupply().call()/(10**18))
+                    with open('./DATA.json','r') as k:
+                        data = (json.load(k))
+                        if (list(data["ETH"]["PRICE"].values())[-1]) != pricee:
+                            data["ETH"]["PRICE"][str(int(time.time()))] = pricee
+                            k.close()
+                            with open('./DATA.json','w') as l:
+                                l.write("")
+                                l.write(json.dumps(data))
+                                print(data)
+                                l.close()
+                        else:
+                            k.close() 
+                    with open('./DATA.json','r') as m:
+                        data = (json.load(m))
+                        if (list(data["ETH"]["TOTALSUPPLY"].values())[-1]) != totalsupply:
+                            data["ETH"]["TOTALSUPPLY"][str(int(time.time()))] = totalsupply
+                            m.close()
+                            with open('./DATA.json','w') as n:
+                                n.write("")
+                                n.write(json.dumps(data))
+                                print(data)
+                                n.close()
+                        else:
+                            m.close()
+    except:
+        print("Error")
---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+gnome-terminal --full-screen -- tmux new-session \; \
+  send-keys 'uvicorn main:app --reload' C-m \; \
+  split-window -v \; \
+  send-keys 'ngrok' C-m \;\
+  split-window -h \; \
+  send-keys 'python3 datafetchETH.py' C-m \; \
+  select-pane -t 0 \; \
+  split-window -h \; \
+  send-keys 'python3 datafetchBSC.py' C-m \;
