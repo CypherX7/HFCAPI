5ef28cb999583c4072b7c6efdacd5d21b79bb844
CypherX7 <cypherx702@gmail.com>
2022-05-29 23:39:18 +0530

    final

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 gnome-terminal --full-screen -- tmux new-session \; \
   send-keys 'uvicorn main:app --reload' C-m \; \
   split-window -v \; \
-  send-keys 'ngrok' C-m \;\
+  send-keys 'ngrok http 8000' C-m \;\
   split-window -h \; \
   send-keys 'python3 datafetchETH.py' C-m \; \
   select-pane -t 0 \; \
