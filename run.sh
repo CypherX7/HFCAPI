1a62b03535221b198317b8995045d4fd67db11d4
CypherX7 <cypherx702@gmail.com>
2022-06-03 15:52:59 +0530

    public network

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux new-session \; \
-  send-keys 'uvicorn main:app --reload' C-m \; \
+  send-keys 'uvicorn main:app --reload --host 0.0.0.0' C-m \; \
   split-window -h \; \
   send-keys 'python3 datafetchETH.py' C-m \; \
   split-window -v \; \
