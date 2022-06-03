469b9be05c0a389d5c7f1145a47fddce14d129aa
CypherX7 <cypherx702@gmail.com>
2022-06-03 15:32:11 +0530

    ngrok removal

--- run.sh
+++ run.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 tmux new-session \; \
   send-keys 'uvicorn main:app --reload' C-m \; \
-  split-window -v \; \
-  send-keys 'neofetch' C-m \;\
   split-window -h \; \
   send-keys 'python3 datafetchETH.py' C-m \; \
-  select-pane -t 0 \; \
-  split-window -h \; \
+  split-window -v \; \
   send-keys 'python3 datafetchBSC.py' C-m \;
