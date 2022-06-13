e10536be61df365cf621d971793e2e780e123935
CypherX7 <cypherx702@gmail.com>
2022-06-13 22:35:40 +0530

    ngrok added

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 tmux new-session \; \
-  send-keys 'uvicorn main:app --reload --host 0.0.0.0' C-m \; \
+  send-keys 'uvicorn main:app --reload' C-m \; \
+  split-window -v \; \
+  send-keys 'ngrok http 8000' C-m \; \
   split-window -h \; \
   send-keys 'python3 datafetchETH.py' C-m \; \
-  split-window -v \; \
+  select-pane -t 0 \; \
+  split-window -h \; \
   send-keys 'python3 datafetchBSC.py' C-m \;
